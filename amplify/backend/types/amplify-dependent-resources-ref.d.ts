5e9b91a4ff98fe1e62126971a6df3d2e80c225c0
Lelluz <alelelli.98@gmail.com>
2022-01-30 20:20:00 +0100

    add s3 storage configuration

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,19 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "mylandscapesgallery2f218b43": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s3mylandscapesgallery": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
